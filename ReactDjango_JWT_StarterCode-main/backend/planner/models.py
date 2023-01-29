8977581b5e9ed83892e9a2ecd200cb12950d7d5d
Niguel Price <niguelprice@yahoo.com>
2023-01-29 12:34:46 -0500

    created planner model for planner app + registered planner app in project settings installed apps section

--- ReactDjango_JWT_StarterCode-main/backend/planner/models.py
+++ ReactDjango_JWT_StarterCode-main/backend/planner/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from authentication.models import User
 # Create your models here.
+
+class PlanOfDay(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    Day = models.CharField(max_length=255)
+    Time = models.CharField(max_length=255)
+    Task = models.CharField(max_length=255)
+
+class PlanOfWeek(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    Day = models.CharField(max_length=255)
+    Task = models.CharField(max_length=255)
+
+class PlanOfMonth(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    Month = models.CharField(max_length=255)
+    Goal = models.CharField(max_length=255)
+
