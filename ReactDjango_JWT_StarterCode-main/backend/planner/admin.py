5ba34ac75cd50026a31dd21644c4c5019b1fce6a
Niguel Price <niguelprice@yahoo.com>
2023-02-01 14:26:10 -0500

    registered planner models with admin site

--- ReactDjango_JWT_StarterCode-main/backend/planner/admin.py
+++ ReactDjango_JWT_StarterCode-main/backend/planner/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PlanOfDay
+from .models import PlanOfWeek
+from .models import PlanOfMonth
 
 # Register your models here.
+admin.site.register(PlanOfDay)
+admin.site.register(PlanOfWeek)
+admin.site.register(PlanOfMonth)
